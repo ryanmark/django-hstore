d8f10098c4ea131f0359cda20cf6f1b7bd991d67
Anthony Lukach <anthonylukach@gmail.com>
2013-11-26 11:43:24 -0700

    Add kludgy check to only serialize data filtered on the  field.  We should find a new way to find out which fields are hstore fields, rather than hard-coding the field name in.

--- django_hstore/managers.py
+++ django_hstore/managers.py
@@ -24,9 +24,9 @@ class HStoreManager(models.Manager):
 
     def filter(self, *args, **kwargs):
         for k,v in kwargs.items():
-            # Only serialize for filters with double underscores (data__contains={'a': '1'}),
-            # rather than equivalence filters (data={'a': '1', 'b': '2'}) where serialization
+            # Only serialize for filters with double underscores (data__contains={'a': 1}),
+            # rather than equivalence filters (data={'a': 1, 'b': 2}) where serialization
             # takes place in DictionaryField.get_prep_value().
-            if len(k.split('__')) > 1:
+            if (len(k.split('__')) > 1) and (k.split('__')[0] == 'data'): # TODO: This check to insure that the arg name is 'data' was added to keep the filter serializer from interfering with the ability to filter other non-hstore fields. We should find a new way to find out which fields are hstore fields, rather than hard-coding the field name in.
                 kwargs[k] = util.json_serialize_dict(v)
         return super(HStoreManager, self).filter(*args, **kwargs)
